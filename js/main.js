a6bd29e45c16b90b69e0588e773c4e7df23e9f16
intro-dog <metvlad3@gmail.com>
2022-12-29 01:01:19 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -39,11 +39,7 @@ function customSelect() {
        list.classList.toggle('custom_select__list--open')
     } 
     }
-    }
-
-customSelect()
-
-const menuBody = document.querySelector('.custom_select__list');
+    const menuBody = document.querySelector('.custom_select__list');
 
     document.addEventListener('click', menu);
 
@@ -55,4 +51,67 @@ const menuBody = document.querySelector('.custom_select__list');
             menuBody.classList.remove('custom_select__list--open')  
         } 
     }
+    }
+
+customSelect()
+
+
+function customSlider(sliderClassName) {
+    let slider = document.querySelector(sliderClassName);
+    let currentSlide = 0;
+    let slides = slider.querySelectorAll('.slider__elem');
+
+    let list = document.createElement('ul');
+    list.classList.add('custom_slider__dots')
+    slider.appendChild(list);
+
+    for(i =0; i < slides.length; i++) {
+    let li = document.createElement('li');
+    let btn = document.createElement('button');
+
+    btn.classList.add('custom_slider__dot');
+    btn.innerText = i + 1;
+    li.appendChild(btn);
+    list.appendChild(li);
+}
+
+let buttons = slider.querySelectorAll('.custom_slider__dot');
+      
+buttons.forEach((btn) => {
+    btn.onclick = function () {
+        btn.classList.add('custom_slider__dot--current');
+        currentSlide = btn.innerText - 1; 
+        changeSlide();
+    };
+});
+
+    let changeSlide = () => {
+        slides.forEach((item) => {
+            item.classList.remove('slider__elem--active');
+        });
+        buttons.forEach((btn) => {
+            btn.classList.remove('custom_slider__dot--current');
+        })
+
+        currentSlide >= slides.length ? (currentSlide = 0) : null;
+        currentSlide <= -1 ? (currentSlide = slides.length - 1) : null;
+
+        buttons[currentSlide].classList.add('custom_slider__dot--current');
+        slides[currentSlide].classList.add('slider__elem--active');
+    };  
     
+    changeSlide();
+
+
+slider.querySelector('.custom_slider__arrow--previous').addEventListener('click', () => {
+    currentSlide--
+    changeSlide();
+});
+slider.querySelector('.custom_slider__arrow--next').addEventListener('click', () => {
+    currentSlide++
+    changeSlide();
+});
+
+}
+
+customSlider('.slider');
